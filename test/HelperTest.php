52af58d3348f267a33e963d7ab7e2148b40a9255
webimpress <contact@webimpress.com>
2019-10-30 19:23:27 +0000

    Fixes rewriting namespaces
    In PHP `\` and `\\` in strings are equal.
    If we want replace two slashes in source file in PHP code we have to use
    4 of them. We should keep number of the slashes the same in output file
    as it is in input.

--- test/HelperTest.php
+++ test/HelperTest.php
@@ -11,10 +11,16 @@ namespace LaminasTest\Migration;
 use Laminas\Migration\Helper;
 use PHPUnit\Framework\TestCase;
 
+use function file_get_contents;
+
 class HelperTest extends TestCase
 {
-    public function edgeCases() : iterable
+    public function edgeCases(): iterable
     {
+        yield 'Example class' => [
+            file_get_contents(__DIR__ . '/TestAsset/TestClass.php'),
+            file_get_contents(__DIR__ . '/TestAsset/TestClass.php.out'),
+        ];
         yield 'Apigility module' => ['ZF\Apigility', 'Apigility'];
         yield 'Apigility documentation module' => ['ZF\Apigility\Documentation', 'Apigility\Documentation'];
         yield 'Apigility modules.config.php' => [
@@ -28,7 +34,7 @@ class HelperTest extends TestCase
      * @param string $string
      * @param string $expected
      */
-    public function testEdgeCases($string, $expected) : void
+    public function testEdgeCases($string, $expected): void
     {
         $this->assertSame($expected, Helper::replace($string));
     }
