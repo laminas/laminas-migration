d7da2aa1f0b9eabeed6bf5095767ee9dffddd397
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-11-19 14:04:34 -0600

    qa: provide tests for Directory::normalizePath

---
+++ test/DirectoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @see       https://github.com/laminas/laminas-migration for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-migration/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-migration/blob/master/LICENSE.md New BSD License
+ */
+
+namespace LaminasTest\Migration;
+
+use Laminas\Migration\Directory;
+use PHPUnit\Framework\TestCase;
+
+class DirectoryTest extends TestCase
+{
+    public function pathsToNormalize() : iterable
+    {
+        yield 'unix'    => ['/home/user/project', '/home/user/project'];
+        yield 'windows' => ['C:\Users\user\project', 'C:/Users/user/project'];
+    }
+
+    /**
+     * @dataProvider pathsToNormalize
+     */
+    public function testNormalizePathNormalizesToUnixPaths(string $original, string $expected) : void
+    {
+        $dir = new Directory();
+        $this->assertSame($expected, $dir->normalizePath($original));
+    }
+}
