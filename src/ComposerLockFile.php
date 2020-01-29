ca2148a481c89cfc87759da0a038e38f858fac5d
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2020-01-29 22:16:54 +0100

    Added new option `--keep-package-versions` to provide a way of keeping locked versions

--- src/ComposerLockFile.php
+++ src/ComposerLockFile.php
@@ -10,7 +10,11 @@ namespace Laminas\Migration;
 
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use function array_column;
+use function array_combine;
 use function file_exists;
+use function file_get_contents;
+use function json_decode;
 use function unlink;
 
 class ComposerLockFile
@@ -20,10 +24,53 @@ class ComposerLockFile
      */
     public function remove($path, SymfonyStyle $io)
     {
-        if (! file_exists($path . '/composer.lock')) {
+        $lockfile = $this->lockfile($path);
+        if (! file_exists($lockfile)) {
             return;
         }
         $io->writeln('<info>Removing composer.lock</info>');
-        unlink($path . '/composer.lock');
+        unlink($lockfile);
+    }
+
+    /**
+     * @param string $path
+     */
+    public function moveLockedVersionsToComposerJson($path, SymfonyStyle $io)
+    {
+        $composerLock = $this->lockfile($path);
+        if (!file_exists($composerLock)) {
+            return;
+        }
+
+        $composerJson = $path . '/composer.json';
+        if (!file_exists($composerJson)) {
+            return;
+        }
+
+        $io->writeln('<info>Moving locked package versions to composer.json</info>');
+
+        $composerLockData = json_decode(file_get_contents($composerLock), true);
+        $lockedPackages = $composerLockData['packages'];
+        $packageVersions = array_combine(array_column($lockedPackages, 'name'), array_column($lockedPackages, 'version'));
+
+        $lockedDevPackages = $composerLockData['packages-dev'];
+        $devPackageVersions = array_combine(array_column($lockedDevPackages, 'name'), array_column($lockedDevPackages, 'version'));
+
+        $composerJsonData = json_decode(file_get_contents($composerJson));
+
+        foreach ($packageVersions as $package => $version) {
+            $composerJsonData['require'][$package] = $version;
+        }
+
+        foreach ($devPackageVersions as $package => $version) {
+            $composerJsonData['require-dev'][$package] = $version;
+        }
+
+        Helper::writeJson($composerJson, $composerJsonData);
+    }
+
+    private function lockfile($path)
+    {
+        return $path . '/composer.lock';
     }
 }
