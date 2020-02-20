2c9fc10ddc45fae247fd7433ed594eab53eeff15
Axel Helmert <axel.helmert@luka.de>
2020-02-20 09:42:14 +0100

    Add tests for edge cases

--- test/PackageVersionsTest.php
+++ test/PackageVersionsTest.php
@@ -25,17 +25,72 @@ class PackageVersionsTest extends TestCase
         );
     }
 
-    public function testShouldBuildFromComposerFiles(): void
+    public function provideComposerFilesTestData(): iterable
     {
-        $versions = PackageVersions::fromComposerFiles([
-            __DIR__ . '/composer-fixtures/installed.json',
-            __DIR__ . '/composer-fixtures/missing.json',
-            __DIR__ . '/composer-fixtures/composer.lock',
-        ]);
+        return [
+            'both-present' => [
+                [
+                    __DIR__ . '/composer-fixtures/installed.json',
+                    __DIR__ . '/composer-fixtures/composer.lock',
+                ],
+                '1.0.2'
+            ],
+            'lock-missing' => [
+                [
+                    __DIR__ . '/composer-fixtures/installed.json',
+                    __DIR__ . '/composer-fixtures/missing-composer.lock',
+                ],
+                '1.0.2'
+            ],
+            'installed-missing' => [
+                [
+                    __DIR__ . '/composer-fixtures/missing-installed.json',
+                    __DIR__ . '/composer-fixtures/composer.lock',
+                ],
+                '1.0.2'
+            ],
+            'both-missing' => [
+                [
+                    __DIR__ . '/composer-fixtures/missing-installed.json',
+                    __DIR__ . '/composer-fixtures/missing-composer.lock',
+                ],
+                'UNKNOWN'
+            ],
+            'only-one-provided' => [
+                [
+                    __DIR__ . '/composer-fixtures/installed.json',
+                ],
+                '1.0.2'
+            ],
+            'empty-list' => [
+                [],
+                '1.0.2'
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideComposerFilesTestData
+     * @param string[] $composerFiles
+     */
+    public function testShouldBuildFromComposerFiles(array $composerFiles, string $expectedVersion): void
+    {
+        $versions = PackageVersions::fromComposerFiles($composerFiles);
 
         self::assertSame(
-            '1.0.2',
+            $expectedVersion,
             $versions->getPackageVersion(PackageVersions::APP_PACKAGE_NAME)
         );
+    }
+
+    public function testShouldAlwaysProvideAppVersion(): void
+    {
+        // Testing such a static, env-based method is not simple. We cannot ensure a specific environment
+        // so it can only check whether the method returns a non-empty string
+
+        $version = PackageVersions::getAppVersion();
+
+        self::assertIsString($version); // Lack of proper return types due to php 5.6 compatibility
+        self::assertNotEmpty($version);
     }
 }
